e877f4c1ca5106fd41daacbfa16e6961c959d291
Anneli <anneli@svaja.se>
2015-01-29 22:08:19 +0100

    Revert "Revert "Multi threading programming course""
    This reverts commit aed25b3d69925e4ae3d8c343e43f4fb06e734162.

---
+++ ServerChat/ServerChat/Form1.cs
@@ -0,0 +1,77 @@
+﻿///Anneli Idnert
+///TGSYS 2013
+///2015-01-13
+///Threads assigment 5, ChatServer
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerChat
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener listner;     /// listner makes the server to conect with the clients
+        private List<User> users = new List<User>();      ///list of users
+        private Socket socket;                        ///sockets are used to give acsses throu right port
+        private delegate void display(string u);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            Task tskSetUp = new Task(SetUp);      ///new threadpool setup
+            tskSetUp.Start();
+        }
+        /// <summary>
+        /// Starting a connection for a chat
+        /// </summary>
+        public void SetUp()
+        {
+         listner = new TcpListener(IPAddress.Parse("127.0.0.1"), 50000);   ///making the client to connect to the server
+         listner.Start();
+         Displaymsg("Waiting for conection...");
+         while (true)
+         {
+             socket = listner.AcceptSocket();     ///making the server to accept the sockets
+             Displaymsg("Client conected!");
+             User u = new User(socket, this );    ///user are a list of users, this is instead for Form1
+             Task tsk = new Task(u.Run);          /// Starting a thread pool
+             tsk.Start();
+             users.Add(u);                        ///Adding the users to the chat
+              
+         }
+        }
+            /// <summary>
+            /// Sending in the message   with a deligate expression
+            /// </summary>
+            /// <param name="msg"></param>
+        public void Displaymsg(string msg)
+        {
+            chatDialog.Invoke(new display((i) => chatDialog.Items.Add(i)), new object[] { msg }); 
+        }
+           /// <summary>
+           /// Button to send the message to the server
+           /// </summary>
+           /// <param name="sender"></param>
+           /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+           foreach (User u in users)
+           {
+               u.SendMesage(txtSendingText.Text);     ///Sending the text to the clients
+               Displaymsg(txtSendingText.Text);     ///showing the instans text in the listbox
+               txtSendingText.Clear();       ///Clearing the text input
+           }
+        }
+    }
+}
---
+++ ServerChat/ServerChat/User.cs
@@ -0,0 +1,66 @@
+﻿///Anneli Idnert
+///TGSYS 2013
+///2015-01-13
+///Threads assigment 5, ChatServer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ServerChat
+{
+    class User
+    {
+
+        private NetworkStream stream;  ///Starting the Stream to make it possble to send text
+        private BinaryReader reader;   ///Reading possibilities
+        private BinaryWriter writer;   /// writing possibiliteis
+        private Form1 frm;             /// form display to show the text in the main from
+        private string response;        ///Texts strings
+       public User( Socket mySocket, Form1 frm)    ///sending the socket and the form in the constructor
+        {
+            stream = new NetworkStream(mySocket);
+            this.frm = frm;
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+        }
+           /// <summary>
+           /// Sending in the mesage from clients
+           /// </summary>
+        public void Run ()
+        {
+           
+          
+
+            while (true)
+            {
+                try
+                {
+                    response = reader.ReadString();      ///Reading the response from the client
+                    frm.Displaymsg(response);         ///Showing the response in the client
+                }
+                catch 
+                {
+                 
+                }
+            }
+        }
+               /// <summary>
+               /// Transfering the message
+               /// </summary>
+               /// <param name="msg"></param>
+          public void SendMesage(string msg)
+          {
+              writer.Write(msg);              ///sending a writen message
+          }
+
+        
+
+    }
+}
