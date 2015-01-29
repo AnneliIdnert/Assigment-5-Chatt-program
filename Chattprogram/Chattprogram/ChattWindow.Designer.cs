0171824b751f89279f155474ac5d706f4f5d5ce6
Anneli <anneli@svaja.se>
2015-01-29 22:06:35 +0100

    Multi threading programming course
    A Client-Server solution for using multi-Threading an assigment for a
    course.

---
+++ Chattprogram/Chattprogram/ChattWindow.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Chattprogram
+{
+    partial class ChatClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lisConversation = new System.Windows.Forms.ListBox();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ok!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lisConversation
+            // 
+            this.lisConversation.FormattingEnabled = true;
+            this.lisConversation.ItemHeight = 20;
+            this.lisConversation.Location = new System.Drawing.Point(52, 96);
+            this.lisConversation.Name = "lisConversation";
+            this.lisConversation.Size = new System.Drawing.Size(436, 184);
+            this.lisConversation.TabIndex = 1;
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(52, 300);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(436, 26);
+            this.txt_input.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcom to the Chattprogram";
+            // 
+            // ChatClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 400);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.lisConversation);
+            this.Controls.Add(this.button1);
+            this.Name = "ChatClient";
+            this.Text = "ChatWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lisConversation;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chattprogram/Chattprogram/ChattWindow.cs
@@ -0,0 +1,78 @@
+﻿///Anneli Idnert
+///TGSYS 2013
+///2015-01-13
+///Threads assigment 5, ChatProgram
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Chattprogram
+{
+    public partial class ChatClient : Form
+    {
+        private TcpClient client;            ///Making it possible to conect to a server
+        private NetworkStream stream;        /// Open up to send messages
+        private BinaryReader reader;         ///Reading the different messages from the server
+        private BinaryWriter writer;        ///sending to different serves
+        private string response;            /// String response
+        private delegate void display(string u);   /// a deligate to be able to send a messages
+        
+
+        public ChatClient()
+        {
+            InitializeComponent();
+            Task tsk = new Task(Run);         ///Starting a threadpool
+            tsk.Start();
+            
+        }
+        /// <summary>
+        /// sending the response to server
+        /// </summary>
+        public void Run()
+        {
+            client = new TcpClient();        
+            client.Connect("127.0.0.1", 50000);   ///Server conections
+            stream = client.GetStream(); ///Open the stream
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+            while (true)
+            {
+                try
+                {
+                    response = reader.ReadString();          /// the response sends throught a deligate
+                    lisConversation.Invoke(new display((i) => lisConversation.Items.Add(i)), new object[] { response }); 
+                }
+                catch 
+                {
+                    
+                    
+                }
+            }
+        }
+                   /// <summary>
+                   /// Button to write the input to the server and other clients
+                   /// </summary>
+                   /// <param name="sender"></param>
+                   /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writer.Write(txt_input.Text);
+            
+        }
+
+
+    }
+}
